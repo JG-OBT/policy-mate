6cf22181831d745ae76e33ab380a4570f9950dda
JG-OBT <jal.gilbertson@gmail.com>
2026-05-08 15:22:30 +0100

    Enable pricing section display

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 import { Document, PricingTier } from './types';
 
 // TOGGLE THIS TO SHOW/HIDE PRICING
-export const SHOW_PRICING_SECTION = false; 
+export const SHOW_PRICING_SECTION = true; 
 
 export const DOCUMENTS: Document[] = [
   { id: '1', title: 'Employee Code of Conduct', path: '/documents/employee-code-of-conduct.pdf' },
